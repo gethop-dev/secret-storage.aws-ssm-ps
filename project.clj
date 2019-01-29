a3af22bc818af93b27e70130407cd10e16f2a645
lucassousaf <lucas.sousa@magnet.coop>
2019-01-29 10:20:57 +0100

    Update clojure to 1.10.0 and add deploy config

--- project.clj
+++ project.clj
@@ -1,16 +1,17 @@
-(defproject magnet/secret-storage.aws-ssm-ps "0.2.0"
+(defproject magnet/secret-storage.aws-ssm-ps "0.3.0"
   :description "Store secret values in AWS Systems Manager Parameter Store"
   :url "https://github.com/magnetcoop/secret-storage.aws-ssm-ps"
   :license {:name "Mozilla Public Licence 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
-  :dependencies [[amazonica "0.3.136" :exclusions [com.amazonaws/aws-java-sdk
+  :min-lein-version "2.8.3"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [amazonica "0.3.136" :exclusions [com.amazonaws/aws-java-sdk
                                                    com.amazonaws/amazon-kinesis-client
                                                    com.amazonaws/dynamodb-streams-kinesis-adapter]]
                  [com.amazonaws/aws-java-sdk-core "1.11.468"]
                  [com.amazonaws/aws-java-sdk-s3 "1.11.468"]
                  [com.amazonaws/aws-java-sdk-ssm "1.11.468"]
-                 [integrant "0.7.0"]
-                 [org.clojure/clojure "1.9.0"]]
+                 [integrant "0.7.0"]]
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
                                       :username :env/clojars_username
                                       :password :env/clojars_password
