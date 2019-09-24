7a9db3cb3e7b4fd32dab656589a74b08912eef82
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2019-09-24 19:52:30 +0200

    Prepare for next development release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject magnet/secret-storage.aws-ssm-ps "0.4.0"
+(defproject magnet/secret-storage.aws-ssm-ps "0.5.0-SNAPSHOT"
   :description "Store secret values in AWS Systems Manager Parameter Store"
   :url "https://github.com/magnetcoop/secret-storage.aws-ssm-ps"
   :license {:name "Mozilla Public Licence 2.0"
