ea16aae583115b0e1e014f090d236877cf72f9ec
spietras <szymon.pietras@magnet.coop>
2022-05-17 15:43:11 +0100

    Prepare for next development release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dev.gethop/secret-storage.aws-ssm-ps "0.5.1"
+(defproject dev.gethop/secret-storage.aws-ssm-ps "0.5.2-SNAPSHOT"
   :description "Store secret values in AWS Systems Manager Parameter Store"
   :url "https://github.com/gethop-dev/secret-storage.aws-ssm-ps"
   :license {:name "Mozilla Public Licence 2.0"
